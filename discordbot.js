26f7218c2934e45a32c3ed13601fba5644081dff
Xn2 <xini540@gmail.com>
2016-08-13 19:24:23 +0200

    added ready message

--- discordbot.js
+++ discordbot.js
@@ -6,6 +6,12 @@ bot.loginWithToken("MjEzNzg3NDI0OTQ4NDg2MTQ1.Co_idw.avAwjKieTDkvWrALiq-UKpqeUJs"
 
 bot.sendMessage("94913717988229120", "Amber Bot started");
 
+bot.on("ready", function (message)
+{
+bot.sendMessage("94913717988229120", "Ambey is ready, Daddy! :heart:");
+bot.sendMessage("214067025755111426", "Amber started.")
+});
+
 bot.on("message", function (message)
 {
 	if (message.author.id === "213787424948486145")
@@ -32,9 +38,10 @@ bot.on("message", function (message)
 
 	if (message.channel.isPrivate === true)
 	{
-		if (message.content === "Hello" && message.author.id === "94913717988229120")
+		if (message.content === "!disconnect" && message.author.id === "94913717988229120")
 		{
-			bot.sendMessage(message, "Hi");
+			bot.sendMessage(message, "Bye Daddy...");
+			bot.logout();
 		}
 	} 
 });
