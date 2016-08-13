7d842fc7551f9056d1494baff2bd12de564d6e93
Xn2 <xini540@gmail.com>
2016-08-13 23:37:51 +0200

    added console colors

--- discordbot.js
+++ discordbot.js
@@ -1,14 +1,15 @@
 var Discord = require ("discord.js");
 
+var colors = require ("colors");
+
 var bot = new Discord.Client();
 
 bot.loginWithToken("MjEzNzg3NDI0OTQ4NDg2MTQ1.Co_idw.avAwjKieTDkvWrALiq-UKpqeUJs");
 
-bot.sendMessage("94913717988229120", "Amber Bot started");
-
 bot.on("ready", function (message)
 {
-console.log("Amber is online!")
+console.log("")
+console.log("Amber is online and ready to rock!".green)
 bot.sendMessage("94913717988229120", "Ambey is ready, Daddy! :heart:");
 bot.sendMessage("214067025755111426", "Amber started.")
 });
